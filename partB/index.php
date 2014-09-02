77079d1cfe4b2f4182fb208bfb734924276b08f7
NABIN PUN <NPun@NABINs-MacBook-Pro.local>
2014-09-02 18:00:19 +1000

    testing form

--- partB/index.php
+++ partB/index.php
@@ -1,3 +1,14 @@
-<?php
- I have just created new php file
-?>
+2	<html> 
+3	  <head> 
+4	    <meta  http-equiv="Content-Type" content="text/html;  charset=iso-8859-1"> 
+5	    <title>Search  Contacts</title> 
+6	  </head> 
+7	  <p><body> 
+8	    <h3>Search  Contacts Details</h3> 
+9	    <p>You  may search either by first or last name</p> 
+10	    <form  method="post" action="search.php?go"  id="searchform"> 
+11	      <input  type="text" name="name"> 
+12	      <input  type="submit" name="submit" value="Search"> 
+13	    </form> 
+14	  </body> 
+15	</html>
